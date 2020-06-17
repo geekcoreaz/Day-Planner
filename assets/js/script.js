54fb9c9872f09f9a8c647eec567302048b388a52
Wesley Stroeber <wesley@geekcore.org>
2020-06-16 23:42:00 -0700

    Added JS functionality, updated CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,51 @@
-//This is the main JavaScript file for the work day planner
+// This is the main JavaScript file for the work day planner.
+let dateTime = moment().format('MMMM Do YYYY, h:mm a');
+let now = moment();
+let sched = {};
+$("#currentDay").append(dateTime);
 
-//This function takes in and displays the local date and time in real-time using moment.js
-function realTime() {
-    var dateTime = moment().format("LLLL");
-    console.log(dateTime);
-    $("#currentDay").append(dateTime);
+
+// onclick event for each save button
+
+$('.saveBtn').click(function () {
+    let text = $(this).siblings(".description").val()
+    let time = $(this).parent().attr("data-time")
+    sched[time] = text;
+    let schedString = JSON.stringify(sched);
+    localStorage.setItem('sched', schedString)
+});
+
+// iIf local storage has data, get it and parse it back to the textarea via Json.
+
+let storage = localStorage.getItem('sched');
+
+if (storage) {
+    sched = JSON.parse(storage);
+} else {
+    for (let i = 0; i <= 17; i++) {
+        sched[i] = ''
+        console.log("Nothing is saved to local storage.")
+    }
+};
+
+// Using "for loop" for attributes and objects and classes defined with css to change the color of the rows.
+
+for (let key in sched) {
+
+    if (moment(key, 'h').isBefore(now)) {
+        $("[data-time=" + key + "]")
+            .find('.description')
+            .val(sched[key])
+            .addClass("past");
+    } else if (moment(key, 'h').isAfter(now)) {
+        $("[data-time=" + key + "]")
+            .find('.description')
+            .val(sched[key])
+            .addClass("future");
+    } else if (moment(key, 'h').isSame(now)) {
+        $("[data-time=" + key + "]")
+            .find('.description')
+            .val(sched[key])
+            .addClass("present")
+    }
 }
-realTime();
-console.log(realTime);
